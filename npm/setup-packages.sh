a872bf03a26f8b6bda1671ad1a1fb1128effb3a7
Jeroen Peeters <contact@jeroenpeeters.be>
2024-05-12 19:36:15 +0200

    chore: deployctl and flyctl for infra management

--- npm/setup-packages.sh
+++ npm/setup-packages.sh
@@ -1,6 +1,10 @@
 setup_packages() {
+  # npm - pnpm
   nvm install lts/*
   nvm alias default lts/*
   npm i -g pnpm
   pnpm i -g wrangler eas-cli firebase-tools nx surge
+
+  # deno
+  deno install -Arf jsr:@deno/deployctl
 }
