d16083114293865f73994f03d291a1bfca408667
Jeroen Peeters <contact@jeroenpeeters.be>
2024-05-12 20:25:26 +0200

    chore: enable auto setup

--- autosetup.sh
+++ autosetup.sh
@@ -10,13 +10,11 @@ cd setup
 
 curl -L https://github.com/jeroenptrs/mac-setup/tarball/main | tar zx
 cd $(ls -d */|head -n 1)
-# ./setup-mac.sh
+./setup-mac.sh
 
-read -p "Do you want to clean up the project? (yes/no) " yn
-case $yn in 
-	yes ) clean_up;;
-	no ) echo exiting...;
-		exit;;
-	* ) echo invalid response;
-		exit 1;;
-esac
+read -p "Do you want to clean up the project? (y/n) " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  clean_up
+fi
