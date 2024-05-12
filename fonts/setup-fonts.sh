01be885924b5c58e1bb22affe58d36b123f142d2
Jeroen Peeters <contact@jeroenpeeters.be>
2024-05-12 20:18:54 +0200

    chore: update font setup script

--- fonts/setup-fonts.sh
+++ fonts/setup-fonts.sh
@@ -3,10 +3,7 @@
 SCRIPT_DIR="$(dirname "$0")"
 
 setup_fonts() {
-  open -b com.apple.FontBook "$SCRIPT_DIR/CommitMono-400-Regular.otf"  
-  open -b com.apple.FontBook "$SCRIPT_DIR/CommitMono-400-Italic.otf"  
-  open -b com.apple.FontBook "$SCRIPT_DIR/CommitMono-700-Regular.otf"  
-  open -b com.apple.FontBook "$SCRIPT_DIR/CommitMono-700-Italic.otf"  
+  cp $SCRIPT_DIR/*.otf $HOME/Library/Fonts/
 }
 
 setup_fonts
